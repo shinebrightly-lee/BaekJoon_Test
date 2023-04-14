4698de869fa618fda70ade3577e460ec2bb4dd5f
shinebrightly <128344191+shinebrightly-lee@users.noreply.github.com>
2023-04-14 23:23:01 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 10.57 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public String solution(String s) {
+        String[] str = s.split(" ");
+        int[] arr = new int[str.length];
+        for(int i = 0; i < str.length; i++){
+            arr[i] = Integer.parseInt(str[i]);
+        }
+        Arrays.sort(arr);
+        return arr[0] + " " + arr[arr.length-1];
+    }
+}
